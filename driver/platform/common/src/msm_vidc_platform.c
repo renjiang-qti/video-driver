5dd0f1c4c28fe4a981b80ad2560bca7d838ee345
Renjiang Han <quic_renjiang@quicinc.com>
2024-06-04 07:32:53 +0530

    video: driver: cleanup the code for set_qp_offset
    Remove unused variables in the set_chroma_qp_index_offset function.
    
    Change-Id: I6c5c986fa69d19803e265e79c42ede4198827cbf
    Signed-off-by: Renjiang Han <quic_renjiang@quicinc.com>

--- driver/platform/common/src/msm_vidc_platform.c
+++ driver/platform/common/src/msm_vidc_platform.c
@@ -3086,14 +3086,8 @@ int msm_vidc_set_chroma_qp_index_offset(void *instance,
 					enum msm_vidc_inst_capability_type cap_id)
 {
 	int rc = 0;
-	u32 width, height;
 	struct msm_vidc_inst *inst = (struct msm_vidc_inst *)instance;
-	struct v4l2_format *f;
 	u32 hfi_value = 0, chroma_qp_offset_mode = 0, chroma_qp = 0;
-
-	f = &inst->fmts[OUTPUT_PORT];
-	width = f->fmt.pix_mp.width;
-	height = f->fmt.pix_mp.height;
 
 	if (inst->capabilities[cap_id].flags & CAP_FLAG_CLIENT_SET)
 		chroma_qp_offset_mode = HFI_FIXED_CHROMAQP_OFFSET;
