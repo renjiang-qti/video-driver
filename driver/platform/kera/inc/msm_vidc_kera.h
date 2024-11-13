19adf41286869cbe87f1f0a81e2a7a3ca1c43d5d
Rajathi S <quic_rajathis@quicinc.com>
2024-11-13 14:15:08 +0530

    Video: Driver: Add platform file for kera target
      - Add platform specific files for kera.
    
    Change-Id: I171328d68b2bd7f697ebf905bb473ab7af2124b4
    Signed-off-by: Rajathi S <quic_rajathis@quicinc.com>

---
+++ driver/platform/kera/inc/msm_vidc_kera.h
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#ifndef _MSM_VIDC_KERA_H_
+#define _MSM_VIDC_KERA_H_
+
+#include "msm_vidc_core.h"
+
+#if defined(CONFIG_MSM_VIDC_SUN)
+int msm_vidc_get_platform_data_kera(struct msm_vidc_core *core);
+int msm_vidc_init_platform_kera(struct msm_vidc_core *core);
+int msm_vidc_deinit_platform_kera(struct msm_vidc_core *core);
+#else
+int msm_vidc_get_platform_data_kera(struct msm_vidc_core *core)
+{
+	return -EINVAL;
+}
+
+int msm_vidc_init_platform_kera(struct msm_vidc_core *core)
+{
+	return -EINVAL;
+}
+
+int msm_vidc_deinit_platform_kera(struct msm_vidc_core *core)
+{
+	return -EINVAL;
+}
+#endif
+
+#endif // _MSM_VIDC_KERA_H_
