CodeLinaro-mirror/le_platform_vendor_opensource_video-driver

video: driver: add overlay change for hamoa
Enable Hamoa overlay support for x1e80100 by registering a new iris
compat, wiring Hamoa platform/vpu in the common compat table, updating
context-bank layout, and allowing probe-time matching in msm_vidc.

Change-Id: Iff7c8fac3014e221308056d3e627c532fdc6ae32
Signed-off-by: Renjiang Han <renjiang@qti.qualcomm.com>

driver/platform/common/src/msm_vidc_platform.c
@@ -74,6 +74,10 @@
 #include "msm_vidc_ravelin.h"
 #include "msm_vidc_ar50lt.h"
 #endif
+#if defined(CONFIG_MSM_VIDC_QLI)
+#include "msm_vidc_hamoa.h"
+#include "msm_vidc_iris3.h"
+#endif
 
 #define CAP_TO_8BIT_QP(a) {          \
 	if ((a) < MIN_QP_8BIT)                 \
@@ -248,6 +252,14 @@ static const struct msm_vidc_compat_handle compat_handle[] = {
 #if defined(CONFIG_MSM_VIDC_HAMOA)
 	{
 		.compat                     = "qcom,x1e80100-vidc",
+		.get_platform_data          = msm_vidc_get_platform_data_hamoa,
+		.init_platform              = msm_vidc_init_platform_hamoa,
+		.init_vpu                  = msm_vidc_init_iris3,
+	},
+#endif
+#if defined(CONFIG_MSM_VIDC_QLI)
+	{
+		.compat                     = "qcom,x1e80100-iris",
 		.get_platform_data          = msm_vidc_get_platform_data_hamoa,
 		.init_platform              = msm_vidc_init_platform_hamoa,
 		.init_vpu                  = msm_vidc_init_iris3,

renjiang-qti/video-driver

driver/platform/common/src/msm_vidc_platform.c
@@ -66,6 +66,10 @@
 #include "msm_vidc_nordau.h"
 #include "msm_vidc_iris36.h"
 #endif
+#if defined(CONFIG_MSM_VIDC_QLI)
+#include "msm_vidc_hamoa.h"
+#include "msm_vidc_iris3.h"
+#endif
 
 #define CAP_TO_8BIT_QP(a) {          \
 	if ((a) < MIN_QP_8BIT)                 \
@@ -163,6 +167,14 @@ static const struct msm_vidc_compat_handle compat_handle[] = {
 #if defined(CONFIG_MSM_VIDC_HAMOA)
 	{
 		.compat                     = "qcom,x1e80100-vidc",
+		.get_platform_data          = msm_vidc_get_platform_data_hamoa,
+		.init_platform              = msm_vidc_init_platform_hamoa,
+		.init_iris                  = msm_vidc_init_iris3,
+	},
+#endif
+#if defined(CONFIG_MSM_VIDC_QLI)
+	{
+		.compat                     = "qcom,x1e80100-iris",
 		.get_platform_data          = msm_vidc_get_platform_data_hamoa,
 		.init_platform              = msm_vidc_init_platform_hamoa,
 		.init_iris                  = msm_vidc_init_iris3,